ba9c87b9bfd85ce3fdd9c4cd10deeec9e8d22e1c
veehz <veehzhz@gmail.com>
2023-04-18 17:22:44 +0800

    feat: show error with red ring

--- src/components/FormComponents/TextInput.tsx
+++ src/components/FormComponents/TextInput.tsx
@@ -18,19 +18,22 @@ export default function TextInput({
     <div className="w-full">
       <div className="px-2 py-1">
         <label htmlFor={id}>{inputName || placeholder}</label>
-        <div className="text-sm text-red-500">{errorMsg}</div>
       </div>
       <input
         type="text"
         className={
           "rounded-md relative block w-full border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" +
-          " disabled:opacity-75 disabled:bg-slate-100"
+          " disabled:opacity-75 disabled:bg-slate-100" +
+          (errorMsg ? " ring-red-500" : "")
         }
         disabled={disabled}
         id={id}
         placeholder={placeholder}
         {...hook}
       ></input>
+      <div className="px-2 py-1">
+        <div className="text-sm text-red-500">{errorMsg}</div>
+      </div>
     </div>
   );
 }
