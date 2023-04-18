4f0b2d4be993bbd5492aa0c612da081fb3282230
veehz <veehzhz@gmail.com>
2023-04-18 18:18:05 +0800

    feat: RadioInputList show errorMsg

--- src/components/FormComponents/RadioInputList.tsx
+++ src/components/FormComponents/RadioInputList.tsx
@@ -1,13 +1,18 @@
 export default function RadioInputList({
   title,
   children,
+  errorMsg,
 }: {
   title?: string;
   children?: React.ReactNode;
+  errorMsg?: string;
 }) {
   return (
     <div className="space-y-0">
-      <div className="px-2 py-1">{title}</div>
+      <div className="px-2 py-1">
+        {title}{" "}
+        <span className="text-sm text-red-500">{errorMsg}</span>
+      </div>
       {children}
     </div>
   );
