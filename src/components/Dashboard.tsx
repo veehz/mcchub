3743f7dd73bcef59b2dea111c885b187265dccae
veehz <veehzhz@gmail.com>
2023-04-20 11:23:14 +0800

    feat: dashboard show buttons depending on role

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,30 +1,52 @@
 import { onAuthStateChanged } from "firebase/auth";
 import Nav from "@/components/Nav";
-import { auth } from "@/firebase.js";
+import { auth, db } from "@/firebase.js";
 import { User as FirebaseUser, sendEmailVerification } from "firebase/auth";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 
 import Button from "@/components/Button";
+import { onValue, ref } from "firebase/database";
 
-export default function Dashboard({ children }: { children: React.ReactNode }) {
+export default function Dashboard({
+  children,
+  pages,
+  rightPages,
+}: {
+  children: React.ReactNode;
+  pages?: Array<[string, string]>;
+  rightPages?: Array<[string, string]>;
+}) {
   const [user, setUser] = useState<FirebaseUser | null | undefined>(undefined);
+  const [role, setRole] = useState<string>("");
   const router = useRouter();
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       if (!user) router.push("/");
+      onValue(
+        ref(db, `role/${user!.uid}`),
+        (snapshot) => {
+          setRole(snapshot.val());
+        },
+        { onlyOnce: true }
+      );
     });
   }, [router]);
 
   return (
     <div>
       <Nav
-        pages = {[
-          ["Dashboard", "/dashboard"],
-        ]}
-        rightPages={[
-          ["Profile", "/profile"],
-        ]}
+        pages={
+          pages ||
+          (role == "student"
+            ? [["Dashboard", "/dashboard"]]
+            : [
+                ["Dashboard", "/dashboard"],
+                ["Students", "/students"],
+                ["Payment", "/payment"],
+              ])
+        }
+        rightPages={rightPages || [["Profile", "/profile"]]}
         user={user?.email}
       />
       <main className="p-3">
