f0367517c0ecf73a5b533a03a104d7c05b73c78f
veehz <veehzhz@gmail.com>
2024-09-22 16:48:07 +0800

    feat: remove console.log in prod

--- next.config.js
+++ next.config.js
@@ -6,6 +6,9 @@ const nextConfig = {
     unoptimized: true,
   },
   basePath: process.env.BASE_PATH == "/" ? "" : process.env.BASE_PATH,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production" ? { exclude: ["error", "warn"] } : false,
+  }
 };
 
 module.exports = nextConfig;
